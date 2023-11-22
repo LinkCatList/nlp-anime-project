248608ba5df38c7a4938cd52588491fe3370a2e6
Robot_chicken <68266302+LinkCatList@users.noreply.github.com>
2023-11-22 18:36:03 +0300

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/about.html")
+	})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/index.html")
+	})
+	fmt.Println("Server is listening...")
+	http.ListenAndServe(":8181", nil)
+}
 
