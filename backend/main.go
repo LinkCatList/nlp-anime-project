248a587589c185f6fc008620114af817dfac5057
Robot_chicken <68266302+LinkCatList@users.noreply.github.com>
2023-11-23 01:13:13 +0300

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	db, err := sql.Open("sqlite3", "test.db")
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("OK")
+	}
+	ok, err := db.Exec("CREATE TABLE USER(ID INTEGER,LOGIN TEXT,PASSWORD TEXT);")
+	if err != nil {
+		panic(err)
+		fmt.Println(ok.LastInsertId())
+	} else {
+		fmt.Println("OK")
+	}
 
-	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "static/about.html")
-	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "static/index.html")
+		http.ServeFile(w, r, "static/register.html")
+	})
+	http.HandleFunc("/postform", func(w http.ResponseWriter, r *http.Request) {
+
+		name := r.FormValue("userlogin")
+		password := r.FormValue("userpassword")
+		fmt.Fprintf(w, "Имя: %s Возраст: %s", name, password)
+		_, err := db.Exec("insert into USER(ID, LOGIN, PASSWORD) values (1, $1, $2)", name, password)
+		if err != nil {
+			panic(err)
+		} else {
+			fmt.Println("OK")
+		}
 	})
 	fmt.Println("Server is listening...")
 	http.ListenAndServe(":8181", nil)
 }
-
