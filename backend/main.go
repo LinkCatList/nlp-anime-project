2191163dc407d8bc578e05321a0951674ef2f8e0
Robot_chicken <68266302+LinkCatList@users.noreply.github.com>
2023-11-23 12:22:39 +0300

    add check exists user

--- backend/main.go
+++ backend/main.go
@@ -35,13 +35,25 @@ func main() {
 
 		name := r.FormValue("userlogin")
 		password := r.FormValue("userpassword")
-		_, err := db.Exec("insert into USER(ID, LOGIN, PASSWORD) values (1, $1, $2)", name, password)
-		if err != nil {
-			panic(err)
-		} else {
-			fmt.Println("OK")
+
+		var count int
+		err1 := db.QueryRow("SELECT COUNT(*) FROM USER WHERE LOGIN = $1", name).Scan(&count)
+		if err1 != nil {
+			panic(err1)
 		}
-		http.ServeFile(w, r, "static/about.html")
+		if count > 0 {
+			fmt.Println("Exists")
+			http.ServeFile(w, r, "static/used.html")
+		} else {
+			fmt.Println("new user ", name)
+			_, err := db.Exec("insert into USER(ID, LOGIN, PASSWORD) values (1, $1, $2)", name, password)
+			if err != nil {
+				panic(err)
+			} else {
+				fmt.Println("OK")
+			}
+			http.ServeFile(w, r, "static/about.html")
+		}
 	})
 	http.HandleFunc("/login.html", func(w http.ResponseWriter, r *http.Request) {
 
