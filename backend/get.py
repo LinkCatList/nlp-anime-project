b1f02b39331752f48d81bb0607d8491eb0d8a721
Robot_chicken <68266302+LinkCatList@users.noreply.github.com>
2023-11-24 09:52:29 +0300

    Create get.py

---
+++ backend/get.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import nltk
+import string
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+nltk.download('punkt')
+
+from keras.models import load_model
+from googletrans import Translator
+
+import json
+
+# выгрузим все необходимое и добавим необходимые функции 
+
+my_model = load_model('14_text_classifier.hdf5') # изменить путь!!!!!!
+
+with open('vocab.json', 'r') as f:
+    mini_vocab = json.load(f)
+
+
+def tokenize_text(text):
+  tokenized_text = nltk.word_tokenize(text)
+  tokens = [i.lower() for i in tokenized_text if ( i not in string.punctuation )]
+  return tokens 
+
+def EncodeTokens(ListTokens, VocabTokens): # токены -> числа
+  res = []
+  res = [VocabTokens.get(word, VocabTokens['<UNKW>']) for word in ListTokens]
+  return [VocabTokens['<START>']] + res
+
+
+########## основная ветка предсказания
+
+labels = np.array(['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Sci-Fi',
+       'Music', 'Kids', 'Slice', 'Hentai'])
+
